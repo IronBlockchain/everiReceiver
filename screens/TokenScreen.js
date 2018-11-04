7f44d1fdbcc42f8cd3788d182ddbca25e572caf3
Hanwen Cheng <heawen.cheng@gmail.com>
2018-11-04 18:42:49 +0100

    finish test token screen

--- screens/TokenScreen.js
+++ screens/TokenScreen.js
@@ -1,16 +1,32 @@
 import React from "react";
 import {FlatList, StyleSheet, View, Text, TouchableOpacity} from "react-native";
 
+const paramsName = 'name'
+
 export default class TokenScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam(paramsName, 'Token Detail'),
+    };
   };
+
+  constructor (props) {
+    super();
+    this.state = {message: 'no message'};
+  }
+
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({message: this.props.navigation.getParam(paramsName, 'No Name')})
+    }, 2000)
+  }
 
   render() {
     const { navigation } = this.props;
     return (
       <View>
-        <Text>{navigation.getParam('name', 'No Name')}</Text>
+        <Text>{navigation.getParam(paramsName, 'No Name')}</Text>
+        <Text>{this.state.message}</Text>
       </View>
     );
   }
